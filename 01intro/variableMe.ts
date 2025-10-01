8d1b8e4216bba58323c4920e88fe54a35157a8b8
harsh <harsh@stackular.com>
2025-10-01 20:37:05 +0530

    hello world in typescript

---
+++ 01intro/variableMe.ts
@@ -0,0 +1,6 @@
+let greetings : string = "Hello Harsh";
+let mynum = 6; //typescipt will detect automatically that it is number
+// mynum.toLowerCase(); //it wont allow us to do this
+console.log(greetings);
+
+export {}
