83b2fe4499540386a8e8ea071f5464b0635db63b
harsh <harsh@stackular.com>
2025-10-01 21:20:50 +0530

    number, boolean and type inference

--- 01intro/variableMe.js
+++ 01intro/variableMe.js
@@ -1,2 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello Harsh";
+var mynum = 6; //typescipt will detect automatically that it is number
+// mynum.toLowerCase(); //it wont allow us to do this
 console.log(greetings);
+//number
+var userId = 224466.3;
+//boolean
+var isLoggedIn = false;
+//type inference
+//not necessary to tell that this is a number as it is too obvious and typescript is smart enough to know , 
+//that okay it is a number and wont allow to use any methods that are used on strings or other data types
+var num = 55.5;
--- 01intro/variableMe.ts
+++ 01intro/variableMe.ts
@@ -3,4 +3,18 @@ let mynum = 6; //typescipt will detect automatically that it is number
 // mynum.toLowerCase(); //it wont allow us to do this
 console.log(greetings);
 
+
+//number
+let userId: number = 224466.3;
+
+//boolean
+let isLoggedIn: boolean = false;
+
+//type inference
+//not necessary to tell that this is a number as it is too obvious and typescript is smart enough to know , 
+//that okay it is a number and wont allow to use any methods that are used on strings or other data types
+
+let num = 55.5;
+// num.toUpperCase(); //if we uncomment this, it will give that squiggly line under toUpperCase method
+
 export {}
