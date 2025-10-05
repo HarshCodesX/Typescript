64dfafa0890a1a0e7dc25c05769d47ca1b30c9c0
harsh <harshguptahb055@gmail.com>
2025-10-05 05:35:31 +0530

    difference between interface and types

--- 02basics/myInterface.ts
+++ 02basics/myInterface.ts
@@ -8,6 +8,15 @@ interface userr {
     getCoupon(couponname: string, value: number): number 
 }
 
+//this is known as reopening of the interface
+interface userr {
+    githubToken: string
+}
+
+interface Adminn extends userr {
+    role: "admin" | "ta" | "learner"
+}
+
 const daksh: userr = {
     dbId: 1234,
     email: "h@h.com",
@@ -18,6 +27,21 @@ const daksh: userr = {
     getCoupon: (name: "first50", val: 50) => {
         return 12;
     },
+    githubToken: "1234hj"
 }
 
 daksh.email = "d@d.com";
+
+const lakshay: Adminn = {
+    dbId: 1234,
+    email: "h@h.com",
+    userId: 123,
+    startTrial: () => {
+        return "trial started"
+    },
+    getCoupon: (name: "first50", val: 50) => {
+        return 12;
+    },
+    githubToken: "1234hj",
+    role: "ta"
+}
