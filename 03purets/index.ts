9c98da8103222fc1c445856d83cb080c1aee7224
harsh <harshguptahb055@gmail.com>
2025-10-06 02:20:13 +0530

    getters and setters in typescript

--- 03purets/index.ts
+++ 03purets/index.ts
@@ -17,12 +17,40 @@
 //another way for declaring classes in typescript
 
 class User {
+    private _couseCount = 1;
     readonly city:string = "Jaipur"
     constructor (public email: string, public name: string, private userId: string){
 
     }
+
+    private deleteToken(){
+        console.log("Token deleted");
+    }
+
+    get getAppleEmail(): string{
+        return `apple ${this.email}`
+    }
+
+    get courseCount(): number{
+        return this._couseCount;
+    }
+
+    // set courseCount(courseNum): void{
+
+    // }
+    //we can not define return type in setters
+
+    set courseCount(courseNum){
+        if(courseNum <= 1){
+            throw new Error("Course count should be more than 1");
+        }
+        else{
+            this._couseCount = courseNum;
+        }
+    }
 }
 
 const daksh = new User("d@d.com", "daksh", "1234");
+// daksh.deleteToken; //can not do that as this function is private
 
 export {}
